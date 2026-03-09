e2c7c8dc8f7ae146ec7a465e498b5e6048cd64df
eischaefer <ethan.i.schaefer@gmail.com>
2026-03-09 11:44:04 -0700

    refactor: Organize modules like pyproj
    Split code from main.py into multiple modules, emulating the organization of the pyproj library. Also add prelim directory (from dev-eis branch), which contains McClernan et al. 7.2 code and an earlier wrapper (now broken) created by EIS. Also add copyright info.

---
+++ src/lgrs/srs/__init__.py
@@ -0,0 +1,6 @@
+# Copyright 2026, Ethan I. Schafer (eschaefer@seti.org)
+#
+# Reuse is permitted under the terms of the license.
+# The AUTHORS file and the LICENSE file are at the
+# top level of this library.
+
