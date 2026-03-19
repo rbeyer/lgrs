d3ac15ff2ef999c022ce0a64dcb70f313e7151df
eischaefer <ethan.i.schaefer@gmail.com>
2026-03-19 14:39:59 -0700

    feat(srs): Extend and refactor convenience code
    `make_lps_or_ltm_crs()` is now `make_lunar_crs()` and supports one additional tier of the simplest calls (e.g., `make_lunar_crs("23N")`). The somewhat complicated argument parsing of `make_lunar_crs()` is now articulated through the dataclass `_CrsParameters`. This improves code readability and enables caching of equivalent (but not identical) calls to `make_lunar_crs()`.

--- src/lgrs/srs/srs.py
+++ src/lgrs/srs/srs.py
@@ -8,8 +8,8 @@
 # region> IMPORT
 ##############################################################################
 # External.
-import abc as _abc
-import functools as _functools
+from __future__ import annotations
+import dataclasses as _dataclasses
 import pyproj as _pyproj
 import re as _re
 
@@ -26,12 +26,106 @@ import lgrs.srs.wkt as _wkt
 _crs_name_pattern = _re.compile(
     "(?i)^"  # Case-insensitive. Match from start.
     "((?P<datum>.+?) ?/ ?)?"  # Datum is optional.
-    "(?P<proj>LPS|LTM)"  # "LPS" or "LTM".
-    "( zone)? "  # "zone" is optional.
+    "((?P<proj>LPS|LTM) )?"  # "LPS" or "LTM" is optional.
+    "(zone )?"  # "zone" is optional.
     "(?P<num>[0-9]+)?"  # Zone number.
     "(?P<n_or_s>[NS])"  # "N" or "S" hemisphere.
     "$"  # Match to end.
 )
+
+# Note: Parsing of arguments to `make_lunar_crs()` is delegated to this
+# class to improve code organization and caching of equivalent calls.
+@_dataclasses.dataclass(kw_only=True, unsafe_hash=True)
+class _CrsParameters:
+    name: _dataclasses.InitVar[str | None] = None
+    proj: str | None = None
+    zone: int | None = None
+    south: bool | None = None
+    ellps: str | None = None
+    extended_ltm: bool = False
+
+    def __post_init__(self, name: str | None) -> None:
+        # Parse `name`, if specified.
+        if name is not None:
+            if (self.proj, self.zone, self.south, self.ellps).count(None) != 4:
+                raise TypeError(
+                    "If `name` is specified, all other arguments "
+                    "except for `extended_ltm` must be `None`"
+                )
+            self._parse_name(name)
+
+        # Resolve and validate compatibility for all arguments.
+        self._resolve_and_validate()
+
+    def _parse_name(self, name: str) -> None:
+        match = _crs_name_pattern.search(name)
+        if match is None:
+            raise TypeError(f"`name` is not in a recognized form: {name!r}")
+        self.ellps = match.group("datum")
+        self.proj = match.group("proj")
+        num = match.group("num")
+        if num is None:
+            self.zone = None
+        else:
+            self.zone = int(num)
+        self.south = (match.group("n_or_s").upper() == "S")
+
+    def _resolve_and_validate(self) -> None:
+        # Check for required parameters.
+        # Note: PROJ defaults to `south=False` for UTM, but that default
+        # is intentionally avoided here in preference of explicitness
+        # and because `south=True` will likely be more used in practice.
+        if self.south is None:
+            raise TypeError("Must specify `south` (or `name`).")
+
+        # Apply defaults and standardize.
+        if self.ellps is None:
+            self.ellps = "IAU_2015:30100"
+        else:
+            self.ellps = self.ellps.upper()
+        if self.proj is None:
+            if self.zone is None:
+                self.proj = "LPS"
+            else:
+                self.proj = "LTM"
+        else:
+            self.proj = self.proj.upper()
+
+        # Check compatibility.
+        if self.proj == "LPS" and self.zone is not None:
+            raise TypeError(
+                f"If `proj` is {self.proj!r}, `zone` must be `None`, not: "
+                f"{self.zone!r}"
+            )
+        elif self.proj == "LTM" and self.zone is None:
+            raise TypeError(
+                f"If `proj` is {self.proj!r}, `zone` must be specified."
+            )
+
+    # Note: Caching this method ensures that equivalent `_CrsParameters`
+    # instances return the same `CRS` instance.
+    @_caching._optionally_cache
+    def make_crs(self) -> CRS:
+        match self.proj:
+            case "LPS":
+                type_ = _wkt.LpsZone
+            case "LTM":
+                type_ = _wkt.LtmZone
+            case _:
+                raise TypeError(f"`proj` not recognized: {self.proj!r}")
+        hemisphere = ("S" if self.south else "N")
+        zone_instance = type_(
+            number=self.zone, hemisphere=hemisphere,
+            extended_ltm=self.extended_ltm, datum_name=self.ellps
+        )
+        crs = CRS.from_wkt(zone_instance.wkt)
+        # TODO: Retain? Document?
+        if self.proj == "LPS":
+            crs.lps_hemisphere = hemisphere
+        else:
+            # Note: Parallels `CRS.utm_zone`.
+            crs.ltm_zone = f"{self.zone}{hemisphere}"
+        return crs
 
 class CRS(_pyproj.CRS, metaclass=_caching._MetaMultiton):
     """
@@ -52,125 +146,6 @@ class CRS(_pyproj.CRS, metaclass=_caching._MetaMultiton):
     False
     """
 
-def make_lps_or_ltm_crs(
-        name: str | None = None, *,
-        proj: str | None = None, zone: int | None = None,
-        south: bool | None = None, ellps: str | None = None,
-        extended_ltm: bool = False,
-) -> CRS:
-    """
-    Return LPS or LTM zone `CRS` using UTM-like `proj.CRS()` arguments.
-
-    See Examples section below.
-
-    Parameters
-    ----------
-    name : str, optional
-        String name of `crs`. If specified, no additional argument may be
-        specified, except for `extended_ltm`.
-    proj : str, optional
-        "LTM" or "LPS". Must be specified, unless `name` is specified.
-    zone : int, optional
-        The LTM zone. Should not be specified for LPS.
-    south : bool, optional
-        Whether `crs` is in the Southern Hemisphere. Must be specified,
-        unless `name` is specified.
-    ellps : str, default="IAU_2015:30100"
-        The name of the `crs` ellipsoid.
-    extended_ltm : bool, default=False
-        Whether to use the extended LTM range of 80-82 degrees.
-
-    Returns
-    -------
-    crs : CRS
-        The LPS or LTM zone CRS instance.
-
-    Examples
-    --------
-    >>> import pyproj
-    >>> utm_crs = pyproj.CRS("WGS84 / UTM zone 23N")
-    >>> ltm_crs = make_lps_or_ltm_crs("IAU_2015:30100 / LTM zone 23N")
-    >>> utm_crs2 = CRS(proj="utm", zone=23, south=False, ellps="WGS84")
-    >>> ltm_crs2 = make_lps_or_ltm_crs(
-    ...    proj="ltm", zone=23, south=False, ellps="IAU_2015:30100"
-    ...    )
-    As a convenience, LPS is also supported.
-    >>> lps_crs = make_lps_or_ltm_crs("IAU_2015:30100 / LPS S")
-    >>> lps_crs2 = make_lps_or_ltm_crs(
-    ...    proj="lps", south=True, ellps="IAU_2015:30100"
-    ...    )
-    As a further convenience, `ellps` defaults to "IAU_2015:30100" and,
-    as with `pyproj.CRS` UTM support, the word "zone" is optional.
-    >>> lps_crs3 = make_lps_or_ltm_crs("LPS S")
-    >>> ltm_crs3 = make_lps_or_ltm_crs("LTM 23N")
-    """
-    # TODO: Should we also make `proj` optional, to support calls like
-    #  `make_lps_or_ltm_crs("S")` and `make_lps_or_ltm_crs("23N")`?
-    #  *Alternatively*, should we drop defaulting `ellps`, to more
-    #  closely parallel `pyproj.CRS()` and what it (may) one day support
-    #  for LTM? Likewise, drop LPS support for the same reason?
-    # TODO: Determine whether to align `crs.name` with `name`. Instead,
-    #  `crs.name` may be "Moon (2015) - Sphere / Ocentric..."
-    # Parse `name`, is specified.
-    if name is not None:
-        if (proj, zone, south, ellps).count(None) != 4:
-            raise TypeError(
-                "If `name` is specified, `zone`, `south`, and `ellps` "
-                "must be `None`."
-            )
-        match = _crs_name_pattern.search(name)
-        if match is None:
-            raise TypeError(f"`name` is not in a recognized form: {name!r}")
-        ellps = match.group("datum")  # *REASSIGNMENT*
-        proj = match.group("proj").upper()  # *REASSIGNMENT*
-        num = match.group("num")
-        if num is None:
-            zone = None  # *REASSIGNMENT*
-        else:
-            zone = int(num)  # *REASSIGNMENT*
-        south = (match.group("n_or_s").upper() == "S")  # *REASSIGNMENT*
-
-    # Otherwise, ensure that required arguments are specified.
-    # Note: PROJ defaults to `south=False` for UTM, but that default is
-    # intentionally avoided here in preference of explicitness and
-    # because `south=True` will likely be more used in practice.
-    elif south is None:
-        raise TypeError("Must specify `south` (or `name`).")
-    elif proj is None:
-        raise TypeError("Must specify `proj` (or `name`).")
-
-    # Default `ellps`, if necessary.
-    if ellps is None:
-        ellps = "IAU_2015:30100"  # *REASSIGNMENT*
-
-    # Determine projection type and test for consistency.
-    proj = proj.upper()  # *REASSIGNMENT*
-    match proj:
-        case "LPS":
-            type_ = _wkt.LpsZone
-            if zone is not None:
-                raise TypeError(
-                    f"If `proj` is {proj!r}, `zone` cannot be specified."
-                )
-        case "LTM":
-            type_ = _wkt.LtmZone
-            if zone is None:
-                raise TypeError(
-                    f"If `proj` is {proj!r}, `zone` must be specified."
-                )
-        case _:
-            raise TypeError(f"`proj` not recognized: {proj!r}")
-
-    # Instantiate and return `CRS`.
-    zone_instance = type_(
-        number=zone, hemisphere=("S" if south else "N"),
-        extended_ltm=extended_ltm, datum_name=ellps.upper()
-    )
-    crs = CRS.from_wkt(zone_instance.wkt)
-    # TODO: Optionally, here, we could assign a `.ltm_zone` (or possibly
-    #  a `.lps_zone`) to parallel `pyproj.CRS.utm_zone`.
-    return crs
-
 class GRS(metaclass=_caching._MetaMultiton):
     """
     Class for gridded reference systems, analogous to `pyproj.CRS`.
@@ -188,13 +163,73 @@ class GRS(metaclass=_caching._MetaMultiton):
         # precaution.
         ...
 
-class BaseSRS(_abc.ABC):
+# Note: Only identical calls are cached here. Compare:
+# `_CrsParameters.make_crs()`.
+@_caching._optionally_cache
+def make_lunar_crs(
+        name: str | None = None, *,
+        proj: str | None = None, zone: int | None = None,
+        south: bool | None = None, ellps: str | None = None,
+        extended_ltm: bool = False,
+) -> CRS:
     """
-    Abstract base class for spatial reference systems.
+    Return LPS or LTM zone `CRS` using UTM-like `proj.CRS()` arguments.
+
+    See Examples section below.
+
+    Parameters
+    ----------
+    name : str, optional
+        String name of `crs`. If specified, all remaining arguments, except
+        for `extended_ltm`, are interpreted from `name` and cannot be
+        independently specified.
+    proj : str, optional
+        "LTM" or "LPS". If not specified, `proj` is inferred from `zone`.
+        That is, `zone=None` implies `proj="LPS"`, otherwise `proj="LTM"`.
+    zone : int, optional
+        The LTM zone. Should not be specified for LPS.
+    south : bool, optional
+        Whether `crs` is in the Southern Hemisphere. Must be specified,
+        unless `name` is specified.
+    ellps : str, default="IAU_2015:30100"
+        The name of the `crs` ellipsoid. Only "IAU_2015:30100" is supported.
+    extended_ltm : bool, default=False
+        Whether to use the extended LTM range of 80-82 degrees.
+
+    Returns
+    -------
+    crs : CRS
+        The LPS or LTM zone `CRS` instance.
+
+    Examples
+    --------
+    >>> import pyproj
+    >>> utm_crs = pyproj.CRS("WGS84 / UTM zone 23N")
+    >>> ltm_crs = make_lunar_crs("IAU_2015:30100 / LTM zone 23N")
+    >>> utm_crs2 = CRS(proj="utm", zone=23, south=False, ellps="WGS84")
+    >>> ltm_crs2 = make_lunar_crs(
+    ...    proj="ltm", zone=23, south=False, ellps="IAU_2015:30100"
+    ...    )
+    As a convenience, LPS is also supported.
+    >>> lps_crs = make_lunar_crs("IAU_2015:30100 / LPS S")
+    >>> lps_crs2 = make_lunar_crs(
+    ...    proj="lps", south=True, ellps="IAU_2015:30100"
+    ...    )
+    As a further convenience, `ellps` defaults to "IAU_2015:30100" and,
+    as with `pyproj.CRS` UTM support, the word "zone" is optional.
+    >>> lps_crs3 = make_lunar_crs("LPS S")
+    >>> ltm_crs3 = make_lunar_crs("LTM 23N")
+    Additionally, `proj` will be inferred if not specified.
+    >>> lps_crs4 = make_lunar_crs("S")
+    >>> ltm_crs4 = make_lunar_crs("23N")
+    >>> lps_crs4.is_exact_same(lps_crs)
+    >>> ltm_crs4.is_exact_same(lps_crs)
     """
-    ...
-BaseSRS.register(_pyproj.CRS)
-BaseSRS.register(GRS)
+    # TODO: Determine whether to align `crs.name` with `name`. Instead,
+    #  `crs.name` may be "Moon (2015) - Sphere / Ocentric..."
+    params = _CrsParameters(**locals())
+    crs = params.make_crs()
+    return crs
 
 
 
