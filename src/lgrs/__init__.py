1d6ff612a4d0765dca6111eed0a717946b497e7b
eischaefer <ethan.i.schaefer@gmail.com>
2026-03-18 18:47:53 -0700

    fix(__init__): Import `easy` via `lgrs` parent

--- src/lgrs/__init__.py
+++ src/lgrs/__init__.py
@@ -11,4 +11,4 @@ from lgrs.transformer import BaseTransformer, GriddedTransformer
 # Note: Unlike in `pyproj`:
 from lgrs.caching import enable_caching
 from lgrs.database import query_lps_and_ltm_crs_info
-from easy import from_gridded, from_geographic, from_lps_or_ltm
+from lgrs.easy import from_gridded, from_geographic, from_lps_or_ltm
