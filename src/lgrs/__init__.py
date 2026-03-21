83536e9bf716923690caa380b3ae4c53e0660168
eischaefer <ethan.i.schaefer@gmail.com>
2026-03-20 18:57:06 -0700

    fix(__init__): Modify imports
    The abstract base class `lgrs.srs.srs.BaseSRS` was removed from `lgrs.srs.srs` as unnecessary in an earlier commit. This commit removes the import of that abstract base class in `__init__`. The name of `lgrs.database.query_lps_and_ltm_crs_info()` will also be changed, soon, to `lgrs.database.query_lunar_crs_info()`. That name change is made in `__init__` in anticipation.

--- src/lgrs/__init__.py
+++ src/lgrs/__init__.py
@@ -5,10 +5,10 @@ __email__ = "rbeyer@seti.org"  # TODO: Decide which, both?
 __version__ = "0.1.0"
 
 # Note: Analogous to `pyproj`:
-from lgrs.srs.srs import BaseSRS, CRS, GRS
+from lgrs.srs.srs import CRS, GRS
 from lgrs.transformer import BaseTransformer, GriddedTransformer
 
 # Note: Unlike in `pyproj`:
 from lgrs.caching import enable_caching
-from lgrs.database import query_lps_and_ltm_crs_info
+from lgrs.database import query_lunar_crs_info
 from lgrs.easy import from_gridded, from_geographic, from_lps_or_ltm
