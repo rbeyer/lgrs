40a0daf3bdb6fedb0f696803031998ec9ed0d401
eischaefer <ethan.i.schaefer@gmail.com>
2026-06-30 13:59:33 -0700

    refactor(`cli`): Use `easy._Adapter`
    Using `easy._Adapter` simplifies the code but does not substantively alter its behavior.

--- src/lgrs/cli.py
+++ src/lgrs/cli.py
@@ -18,7 +18,6 @@
 # region> IMPORT
 ###############################################################################
 # Standard.
-import functools as _functools
 import inspect as _inspect
 import pathlib as _pathlib
 import re as _re
@@ -30,6 +29,7 @@ import typing as _typing
 import typer as _typer
 
 # Internal.
+import lgrs.easy as _easy
 
 # endregion
 ###############################################################################
@@ -49,15 +49,18 @@ _app = _typer.Typer(
 def _prep_for_cli(
     func: _types.FunctionType, examples: str
 ) -> _types.FunctionType:
-    # Extract parameter info from numpy-styled docstring.
-    # Note: Avoid using `numpydoc` module, due to dependencies.
-    params_pattern = _re.compile("(?ms)^Parameters *$.*?^ *$")
-    params_block_match = params_pattern.search(func.__doc__)
+    # Parse parameter names and descriptions from numpy-styled
+    # docstring.
+    # Note: Avoid adding `numpydoc` as a dependency.
+    adapter = _easy._Adapter(func)
+    params_block_match = adapter.get_doc_section_match("Parameters")
     param_name_to_desc = {}
     for match in _re.finditer(
         r"(?m)^(?P<pname>[a-z_]+) : .*\n(?P<pdesc>(    .*\n)+)",
         params_block_match.group(),
     ):
+        # Note: The parameter type description is intentionally not
+        # retained, as the stricter type hint is more precise.
         param_name = match.group("pname")
         raw_desc = match.group("pdesc")
         param_desc = _textwrap.dedent(raw_desc).strip().replace("\n", " ")
@@ -65,25 +68,18 @@ def _prep_for_cli(
 
     # Annotate each parameter.
     sig = _inspect.signature(func)
-    new_annotations = func.__annotations__.copy()
     for pname, pdesc in param_name_to_desc.items():
-        old_ptype = new_annotations[pname]
+        old_ptype = adapter.annotations[pname]
         if sig.parameters[pname].default is sig.empty:
             typer_typ = _typer.Argument
         else:
             typer_typ = _typer.Option
         new_ptype = _typing.Annotated[old_ptype, typer_typ(help=pdesc)]
-        new_annotations[pname] = new_ptype
+        adapter.annotations[pname] = new_ptype
 
-    # Remove "Parameters" and "Examples" section from docstring.
-    # Note: "Parameters" is now redundant (with potentially confusing
-    # types) and "Examples" may be confusing due to call signature
-    # differences.
-    old_doc = func.__doc__
-    no_params_doc = _re.sub(params_pattern, "", old_doc, count=1)
-    examples_pattern = _re.compile(r"(?ms)^Examples *$.*?((^ *$)|\Z)")
-    excerpted_doc = _re.sub(examples_pattern, "", no_params_doc, count=1)
-    clean_doc = _re.sub(r"(?m)(^ *\n){2,}", r"\n", excerpted_doc).strip()
+    # Remove "Parameters" section as redundant, with potentially
+    # confusing type descriptions.
+    adapter.replace_doc_section("Parameters")
 
     # Add new examples to docstring.
     populated_examples = examples.format(
@@ -93,21 +89,10 @@ def _prep_for_cli(
         )
     )
     clean_examples = _textwrap.dedent(populated_examples).strip()
-    new_doc = clean_doc.strip() + "\n\n" + clean_examples
+    adapter.replace_doc_section("Examples", clean_examples)
 
     # Create a (near) copy of `func`.
-    new = _types.FunctionType(
-        func.__code__,
-        func.__globals__,
-        func.__name__,
-        func.__defaults__,
-        func.__closure__,
-    )
-    _functools.update_wrapper(new, func)
-    del new.__wrapped__
-    new.__annotations__ = new_annotations
-    new.__doc__ = new_doc
-    new.__kwdefaults__ = func.__kwdefaults__
+    new = adapter.make_new_func()
 
     # Perform final preparation and return.
     out = _app.command(no_args_is_help=True)(new)
@@ -121,8 +106,6 @@ def _prep_for_cli(
 # from_lps_or_ltm = _prep_for_cli(
 #     _easy.from_lps_or_ltm,
 #     """
-#     Examples
-#     --------
 #     {cmd} 488590 608480
 #     (zone="A", area="ZS", easting=13590, northing=8480,
 #      string="AZS1359008480")
