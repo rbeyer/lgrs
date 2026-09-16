0f8823142b255811238a7039d6cee30b0af948e5
eischaefer <ethan.i.schaefer@gmail.com>
2026-09-16 12:02:36 -0700

    feature(`wkt`): Formalize WKTs
    Made several changes to support formal WKTs, including: use of integers (rather than equivalent floats) where appropriate, use of straight (rather than angled) quotation marks, updating of WKT name and `CONVERSION` name, correction of LPS WKT `AXIS` contents, adoption of a universal `SCOPE` and `REMARK` boilerplates, and auto-generation of `AREA` content. WKT name and `CONVERSION` name also now reflect variants (e.g., a WKT representing an extended LTM zone, a hemisphere LPS region).

--- src/lgrs/srs/wkt.py
+++ src/lgrs/srs/wkt.py
@@ -36,6 +36,7 @@ import abc as _abc
 import dataclasses as _dataclasses
 import functools as _functools
 import itertools as _itertools
+import re as _re
 import typing as _typing
 
 # Internal.
@@ -48,29 +49,34 @@ import lgrs.caching as _caching
 # Note: If any of these variables are modified from their M2025 values,
 # additional changes to the code will likely be necessary.
 # Note: See Tables 4 and 5 of M2025 for most of these variables.
+# Note: Integer values should be represented as integers, for
+# conventional formatting in WKTs.
 
 # * GENERAL VALUES. ───────────────────────────────────────────────────
 # Datum.
 DATUM_NAME = "IAU_2015:30100"
-DATUM_WKT_ID = "ID[“IAU”,30100,2015]"
+DATUM_WKT_ID = 'ID["IAU",30100,2015]'
 
 # Shape parameters.
-LUNAR_RADIUS: float = 1_737_400.0  # `a` in M2025 (meters)
+LUNAR_RADIUS: float = 1_737_400  # `a` in M2025 (meters)
+
+# Other.
+REMARK = "Source of projection parameters: https://doi.org/10.3133/tm11E1"
 
 # * LTM VALUES. ───────────────────────────────────────────────────────
 # Boundaries.
-LTM_EXTENDED_MAX_ABSOLUTE_LATITUDE: float = 82.0  # (degrees)
-LTM_UNEXTENDED_MAX_ABSOLUTE_LATITUDE: float = 80.0  # (degrees)
+LTM_EXTENDED_MAX_ABSOLUTE_LATITUDE: float = 82  # (degrees)
+LTM_UNEXTENDED_MAX_ABSOLUTE_LATITUDE: float = 80  # (degrees)
 
 # False easting and northing.
-LTM_FALSE_EASTING: float = 250_000.0  # `F_E` in M2025 (meters)
-LTM_N_FALSE_NORTHING: float = 0.0  # `F_N` in M2025 (meters)
-LTM_S_FALSE_NORTHING: float = 2_500_000.0  # `F_N` in M2025 (meters)
+LTM_FALSE_EASTING: float = 250_000  # `F_E` in M2025 (meters)
+LTM_N_FALSE_NORTHING: float = 0  # `F_N` in M2025 (meters)
+LTM_S_FALSE_NORTHING: float = 2_500_000  # `F_N` in M2025 (meters)
 
 # Other parameters.
 LTM_CENTRAL_SCALE_FACTOR: float = 0.999  # `k_0` in M2025 (exact, unitless)
-LTM_LATITUDE_OF_PROJECTION_AXIS: float = 0.0  # `phi_0` in M2025 (degrees)
-LTM_ZONE_HALF_WIDTH: float = 4.0  # `W` in M2025 (degrees)
+LTM_LATITUDE_OF_PROJECTION_AXIS: float = 0  # `phi_0` in M2025 (degrees)
+LTM_ZONE_HALF_WIDTH: float = 4  # `W` in M2025 (degrees)
 
 # * LPS VALUES. ───────────────────────────────────────────────────────
 # Projection IDs.
@@ -78,79 +84,27 @@ LPS_N_ID = 7190092
 LPS_S_ID = 7190091
 
 # False easting and northing.
-LPS_FALSE_EASTING: float = 500_000.0  # `F_E` in M2025 (meters)
-LPS_FALSE_NORTHING: float = 500_000.0  # `F_N` in M2025 (meters)
+LPS_FALSE_EASTING: float = 500_000  # `F_E` in M2025 (meters)
+LPS_FALSE_NORTHING: float = 500_000  # `F_N` in M2025 (meters)
 
 # Other parameters.
 LPS_CENTRAL_SCALE_FACTOR: float = 0.994  # `k_0` in M2025 (exact, unitless)
-LPS_N_LATITUDE_OF_PROJECTION_ORIGIN: float = (
-    +90.0
-)  # `phi_0` in M2025 (degrees)
-LPS_S_LATITUDE_OF_PROJECTION_ORIGIN: float = (
-    -90.0
-)  # `phi_0` in M2025 (degrees)
-LPS_LONGITUDE_OF_PROJECTION_ORIGIN: float = (
-    0.0  # `lambda_0` in M2025 (degrees)
-)
+LPS_N_LATITUDE_OF_PROJECTION_ORIGIN: float = +90  # `phi_0` in M2025 (degrees)
+LPS_S_LATITUDE_OF_PROJECTION_ORIGIN: float = -90  # `phi_0` in M2025 (degrees)
+LPS_LONGITUDE_OF_PROJECTION_ORIGIN: float = 0  # `lambda_0` in M2025 (degrees)
 
 
 # endregion
 ###############################################################################
 # region> TEMPLATES
 ###############################################################################
-# TODO: Add user-facing option to generate WKT directly.
-# TODO: Add `MERIDIAN`, `ANGLEUNIT`, "Northing (Y)", "Easting (X)",
-#  normalize to straight quotes.
-# Below: Format taken from p. 36 of M2025. Only deviation from M2025 is
-# the addition of USAGE, which parallels that for the LTM WKT in the
-# current module.
-_lps_usage_area_prefix = "Lunar Polar Stereographic Zone LPS_"
+# Below: Format inspired by p. 36 of M2025 but refined based on the WKT
+# standard and common practice. The placeholder `ID["USGS",...]` is also
+# removed.
+# Note: Keep `_lps_regex` updated in tandem with `CONVERSION`.
+_lps_regex = _re.compile("Lunar Polar Stereographic (.)")
 _format_lps_wkt = f"""
-PROJCRS[“Moon (2015) - Sphere / Ocentric / {{north_or_south}} Polar”,
-  BASEGEOGCRS[“Moon (2015) - Sphere / Ocentric”,
-    DATUM[“Moon (2015) - Sphere”,
-      ELLIPSOID[“Moon (2015) - Sphere”,{LUNAR_RADIUS},0,
-        LENGTHUNIT[“metre”,1]]],
-    PRIMEM[“Reference Meridian”,0,
-      ANGLEUNIT[“degree”,0.0174532925199433]],
-    {DATUM_WKT_ID}],
-  CONVERSION[“{{north_or_south}} Polar”,
-    METHOD[“Polar Stereographic (variant A)”,
-      ID[“EPSG”,9810]],
-    PARAMETER[“Latitude of natural origin”,{{lat_origin}},
-      ANGLEUNIT[“degree”,0.0174532925199433],
-      ID[“EPSG”,8801]],
-    PARAMETER[“Longitude of natural origin”,{LPS_LONGITUDE_OF_PROJECTION_ORIGIN},
-      ANGLEUNIT[“degree”,0.0174532925199433],
-      ID[“EPSG”,8802]],
-    PARAMETER[“Scale factor at natural origin”,{LPS_CENTRAL_SCALE_FACTOR},
-      SCALEUNIT[“unity”,1],
-      ID[“EPSG”,8805]],
-    PARAMETER[“False easting”,{LPS_FALSE_EASTING},
-      LENGTHUNIT[“metre”,1],
-      ID[“EPSG”,8806]],
-    PARAMETER[“False northing”,{LPS_FALSE_NORTHING},
-      LENGTHUNIT[“metre”,1],
-      ID[“EPSG”,8807]]],
-  CS[Cartesian,2],
-    AXIS[“(E)”,east,
-      ORDER[1],
-      LENGTHUNIT[“metre”,1]],
-    AXIS[“(N)”,north,
-      ORDER[2],
-      LENGTHUNIT[“metre”,1]],
-  USAGE[
-    AREA["{_lps_usage_area_prefix}{{hemisphere}}."],
-    {{bbox_string}}],
-ID[“USGS”,{{id_num}},{{hemisphere}}]]]
-""".strip().format  # noqa: E501
-
-# Below: Format taken from p. 23 of M2025. Only deviations from M2025
-# are that SCOPE is replaced with AREA, which seems better suited for
-# the content, and PROJCRS name is extended to include zone number.
-_ltm_usage_area_prefix = "Lunar Transverse Mercator Zone LTM_"
-_format_ltm_wkt = f"""
-PROJCRS["Moon (2015) - Sphere / Ocentric / Transverse Mercator / LTM zone {{zone_number}}{{hemisphere}}",
+PROJCRS["Moon (2015) - Sphere / Ocentric / {{prefix}}LPS {{north_or_south}}{{suffix}}",
   BASEGEOGCRS["Moon (2015) - Sphere / Ocentric",
     DATUM["Moon (2015) - Sphere",
       ELLIPSOID["Moon (2015) - Sphere",{LUNAR_RADIUS},0,
@@ -158,8 +112,58 @@ PROJCRS["Moon (2015) - Sphere / Ocentric / Transverse Mercator / LTM zone {{zone
     PRIMEM["Reference Meridian",0,
       ANGLEUNIT["degree",0.0174532925199433]],
     {DATUM_WKT_ID}],
-  CONVERSION["transverse Mercator",
-    METHOD["transverse Mercator",
+  CONVERSION["{{prefix}}Lunar Polar Stereographic {{north_or_south}}{{suffix}}",
+    METHOD["Polar Stereographic (variant A)",
+      ID["EPSG",9810]],
+    PARAMETER["Latitude of natural origin",{{lat_origin}},
+      ANGLEUNIT["degree",0.0174532925199433],
+      ID["EPSG",8801]],
+    PARAMETER["Longitude of natural origin",{LPS_LONGITUDE_OF_PROJECTION_ORIGIN},
+      ANGLEUNIT["degree",0.0174532925199433],
+      ID["EPSG",8802]],
+    PARAMETER["Scale factor at natural origin",{LPS_CENTRAL_SCALE_FACTOR},
+      SCALEUNIT["unity",1],
+      ID["EPSG",8805]],
+    PARAMETER["False easting",{LPS_FALSE_EASTING},
+      LENGTHUNIT["metre",1],
+      ID["EPSG",8806]],
+    PARAMETER["False northing",{LPS_FALSE_NORTHING},
+      LENGTHUNIT["metre",1],
+      ID["EPSG",8807]]],
+  CS[Cartesian,2],
+    AXIS["easting (X)",{{other_north_or_south_lower}},
+      MERIDIAN[90,
+        ANGLEUNIT["degree",0.0174532925199433]],
+      ORDER[1],
+      LENGTHUNIT["metre",1]],
+    AXIS["northing (Y)",{{other_north_or_south_lower}},
+      MERIDIAN[0,
+        ANGLEUNIT["degree",0.0174532925199433]],
+      ORDER[2],
+      LENGTHUNIT["metre",1]],
+  USAGE[
+    SCOPE["Navigation and mapping."],
+    {{area_string}},
+    {{bbox_string}}],
+  REMARK["{REMARK}"]]
+""".strip().format  # noqa: E501
+
+# Below: Format inspired by p. 23 of M2025 but refined based on the WKT
+# standard and common practice. The placeholder `ID["USGS",...]` is also
+# # removed.
+# Note: Keep `_ltm_regex` updated in tandem with `CONVERSION`.
+_ltm_regex = _re.compile("Lunar Transverse Mercator Zone ([0-9]+.)")
+_format_ltm_wkt = f"""
+PROJCRS["Moon (2015) - Sphere / Ocentric / {{prefix}}LTM {{zone_number}}{{hemisphere}}{{suffix}}",
+  BASEGEOGCRS["Moon (2015) - Sphere / Ocentric",
+    DATUM["Moon (2015) - Sphere",
+      ELLIPSOID["Moon (2015) - Sphere",{LUNAR_RADIUS},0,
+        LENGTHUNIT["metre",1]]],
+    PRIMEM["Reference Meridian",0,
+      ANGLEUNIT["degree",0.0174532925199433]],
+    {DATUM_WKT_ID}],
+  CONVERSION["{{prefix}}Lunar Transverse Mercator Zone {{zone_number}}{{hemisphere}}{{suffix}}",
+    METHOD["Transverse Mercator",
       ID["EPSG",9807]],
     PARAMETER["Latitude of natural origin",{LTM_LATITUDE_OF_PROJECTION_AXIS},
       ANGLEUNIT["degree",0.0174532925199433],
@@ -184,8 +188,10 @@ PROJCRS["Moon (2015) - Sphere / Ocentric / Transverse Mercator / LTM zone {{zone
       ORDER[2],
       LENGTHUNIT["metre",1]],
   USAGE[
-    AREA["{_ltm_usage_area_prefix}{{zone_number}}{{hemisphere}}."],
-    {{bbox_string}}]]
+    SCOPE["Navigation and mapping."],
+    {{area_string}},
+    {{bbox_string}}],
+  REMARK["{REMARK}"]]
 """.strip().format  # noqa: E501
 
 
@@ -247,6 +253,9 @@ class BaseZone(metaclass=_caching._AbstractMetaMultiton):
         _validate_constraints(**self.__dict__)
 
     # * UTILITIES. ────────────────────────────────────────────────────
+    @_abc.abstractmethod
+    def _get_area_string(self, **kwargs) -> str: ...
+
     def _get_bbox_string(self) -> str:
         return (
             f"BBOX[{self.minimum_latitude},{self.minimum_longitude},"
@@ -271,9 +280,9 @@ class BaseZone(metaclass=_caching._AbstractMetaMultiton):
     @_functools.cached_property
     def absolute_ltm_limit(self) -> float:
         if self.global_lps:
-            return 0.0
+            return 0
         elif self.global_ltm:
-            return 90.0
+            return 90
         elif self.extended_ltm:
             return LTM_EXTENDED_MAX_ABSOLUTE_LATITUDE
         else:
@@ -313,21 +322,54 @@ class LpsZone(BaseZone):
         self._validate_datum_name()
         self._validate_hemisphere()
 
+    # * UTILITIES. ────────────────────────────────────────────────────
+    def _get_area_string(
+        self, lat_sign: str, north_or_south: str, **ignore
+    ) -> str:
+        polar_region = "polar region"
+        if self.absolute_ltm_limit == LTM_UNEXTENDED_MAX_ABSOLUTE_LATITUDE:
+            end = "."
+        elif self.absolute_ltm_limit == LTM_EXTENDED_MAX_ABSOLUTE_LATITUDE:
+            end = (
+                ", though valid from "
+                f"{lat_sign}{LTM_UNEXTENDED_MAX_ABSOLUTE_LATITUDE}° latitude "
+                "in the primary range."
+            )
+        elif self.absolute_ltm_limit == 0:
+            end = (
+                ", including the non-standard range equatorward of "
+                f"{lat_sign}{LTM_UNEXTENDED_MAX_ABSOLUTE_LATITUDE}° latitude."
+            )
+            lat_sign = ""  # *REASSIGNMENT*
+            polar_region = "hemisphere"  # *REASSIGNMENT*
+        else:
+            raise TypeError(
+                "`.absolute_ltm_limit` is not supported: "
+                f"{self.absolute_ltm_limit!r}"
+            )
+        return (
+            f'AREA["'
+            f"{north_or_south}ern {polar_region} of the Moon - "
+            f"{north_or_south.lower()} of "
+            f"{lat_sign}{self.absolute_ltm_limit}° latitude{end}"
+            f'"]'
+        )
+
     # * ATTRIBUTES. ───────────────────────────────────────────────────
-    maximum_longitude: _typing.ClassVar = 180.0
-    minimum_longitude: _typing.ClassVar = -180.0
+    maximum_longitude: _typing.ClassVar = 180
+    minimum_longitude: _typing.ClassVar = -180
 
     @_functools.cached_property
     def maximum_latitude(self) -> float:
         if self.hemisphere == "N":
-            return 90.0
+            return 90
         else:
             return -self.absolute_ltm_limit
 
     @_functools.cached_property
     def minimum_latitude(self) -> float:
         if self.hemisphere == "S":
-            return -90.0
+            return -90
         else:
             return self.absolute_ltm_limit
 
@@ -342,14 +384,29 @@ class LpsZone(BaseZone):
     def wkt(self) -> str:
         hemisphere = self.hemisphere
         if hemisphere == "S":
+            lat_sign = "-"
             north_or_south = "South"
+            other_north_or_south = "North"
             lat_origin = LPS_S_LATITUDE_OF_PROJECTION_ORIGIN
             id_num = LPS_S_ID
         else:
+            lat_sign = "+"
             north_or_south = "North"
+            other_north_or_south = "South"
             lat_origin = LPS_N_LATITUDE_OF_PROJECTION_ORIGIN
             id_num = LPS_N_ID
+        other_north_or_south_lower = other_north_or_south.lower()
+        area_string = self._get_area_string(
+            lat_sign=lat_sign, north_or_south=north_or_south
+        )
         bbox_string = self._get_bbox_string()
+        prefix = ""
+        if self.global_lps:
+            suffix = " (unrestricted)"
+        elif self.extended_ltm:
+            suffix = " (exclusive)"
+        else:
+            suffix = ""
         wkt = _format_lps_wkt(**locals())
         return wkt
 
@@ -370,6 +427,41 @@ class LtmZone(BaseZone):
             raise TypeError(
                 f"`number` must be in the range [1, 45], not: {self.number!r}"
             )
+
+    # * UTILITIES. ────────────────────────────────────────────────────
+    def _get_area_string(
+        self, lat_sign: str, north_or_south: str, **ignore
+    ) -> str:
+        if self.absolute_ltm_limit == LTM_UNEXTENDED_MAX_ABSOLUTE_LATITUDE:
+            end = (
+                ", though valid to "
+                f"{lat_sign}{LTM_EXTENDED_MAX_ABSOLUTE_LATITUDE}° latitude "
+                "in the extended range."
+            )
+        elif self.absolute_ltm_limit == LTM_EXTENDED_MAX_ABSOLUTE_LATITUDE:
+            end = (
+                ", including the extended range poleward of "
+                f"{lat_sign}{LTM_UNEXTENDED_MAX_ABSOLUTE_LATITUDE}° latitude."
+            )
+        elif self.absolute_ltm_limit == 90:
+            end = (
+                ", including the non-standard range poleward of "
+                f"{lat_sign}{LTM_EXTENDED_MAX_ABSOLUTE_LATITUDE}° latitude."
+            )
+        else:
+            raise TypeError(
+                "`.absolute_ltm_limit` is not supported: "
+                f"{self.absolute_ltm_limit!r}"
+            )
+        return (
+            f'AREA["'
+            f"Between {self.minimum_longitude:+}° and "
+            f"{self.maximum_longitude:+}° longitude, "
+            f"{north_or_south.lower()}ern hemisphere "
+            "between the equator and "
+            f"{lat_sign}{self.absolute_ltm_limit}° latitude{end}"
+            f'"]'
+        )
 
     # * ATTRIBUTES. ───────────────────────────────────────────────────
     @_functools.cached_property
@@ -422,7 +514,25 @@ class LtmZone(BaseZone):
         hemisphere = self.hemisphere
         center_longitude = self.center_longitude
         false_northing = self.false_northing
+        if hemisphere == "S":
+            lat_sign = "-"
+            north_or_south = "South"
+        else:
+            lat_sign = "+"
+            north_or_south = "North"
+        area_string = self._get_area_string(
+            lat_sign=lat_sign, north_or_south=north_or_south
+        )
         bbox_string = self._get_bbox_string()
+        if self.global_ltm:
+            prefix = ""
+            suffix = " (unrestricted)"
+        elif self.extended_ltm:
+            prefix = "Extended "
+            suffix = ""
+        else:
+            prefix = ""
+            suffix = ""
         wkt = _format_ltm_wkt(**locals())
         return wkt
 
