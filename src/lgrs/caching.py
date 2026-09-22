08321e4edc39d3b448a0ec8315c9dcaeb4127bff
eischaefer <ethan.i.schaefer@gmail.com>
2026-09-22 15:36:51 -0700

    refactor(`caching`): Use `uncache_cousin_group()`
    Modify code to call (new, with recent commit) `BaseCoordinate.uncache_cousin_group()`.

--- src/lgrs/caching.py
+++ src/lgrs/caching.py
@@ -187,9 +187,7 @@ def enable_caching(enable: bool = True, *, clear: bool = False) -> None:
     if clear:
         _cache.clear()
         for coord in _coord_weak_set:
-            record = coord.__dict__.get("_cache_key_to_cousins", None)
-            if record is not None:
-                record.clear()
+            coord.uncache_cousin_group()
 
 
 # endregion
